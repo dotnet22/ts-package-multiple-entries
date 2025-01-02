59befee7b0a73552b478d0263f9be1f3b768e53d
sukhdev <the.sukhdev@gmail.com>
2025-01-02 22:14:41 +0530

    milestone achived

---
+++ src/common/index.ts
@@ -0,0 +1,3 @@
+export function FromCommon() {
+    console.log("from common");
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     //entryPoints: ["src/index.ts"],
-    entryPoints: ["src/**/*.ts"],
+    //entryPoints: ["src/**/*.ts"],
+    entryPoints: ["src/index.ts", "src/components/**/*.ts", "src/utils/**/*.ts"],
     format: ["esm"],
     dts: true,
     outDir: "dist",
