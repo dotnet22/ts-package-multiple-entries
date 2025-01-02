2a1e7168383e60a654cd91a6662c4b04108e25b7
sukhdev <the.sukhdev@gmail.com>
2025-01-02 22:05:24 +0530

    wow

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entryPoints: ["src/index.ts"],
-    format: ["cjs", "esm"],
+    //entryPoints: ["src/index.ts"],
+    entryPoints: ["src/**/*.ts"],
+    format: ["esm"],
     dts: true,
     outDir: "dist",
     clean: true,
+    treeshake: true,
+    splitting: true,
 });
